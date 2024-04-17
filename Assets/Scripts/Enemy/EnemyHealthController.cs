8870ae060ac9d1a8a0160478927dbc714a95c7ed
Mertin H <mhoeger@gmx.ch>
2024-04-17 20:21:44 +0200

    Add Sound Effects/Animation for Destroyed Enemies #8

--- Assets/Scripts/Enemy/EnemyHealthController.cs
+++ Assets/Scripts/Enemy/EnemyHealthController.cs
@@ -6,9 +6,38 @@ namespace Enemy
     [RequireComponent(typeof(Collider))]
     public class EnemyHealthController : MonoBehaviour, IDamageable
     {
+
+        private ParticleSystem deathEffect;
+        private GameObject whitebox;
+        private GameObject gun;
+        private AudioSource deathSound;
+
+        private void Awake()
+        {
+            deathEffect = transform.Find("DeathEffect")?.GetComponent<ParticleSystem>();
+            whitebox = transform.Find("Whitebox")?.gameObject;
+            gun = transform.Find("Gun")?.gameObject;
+            deathSound = GetComponent<AudioSource>();
+        }
+
         public void ApplyDamage()
         {
-           Destroy(gameObject);
+            if (whitebox != null)
+                Destroy(whitebox);
+
+            if (gun != null)
+                Destroy(gun);
+
+            if (deathEffect && !deathEffect.isPlaying)
+                deathEffect.Play();
+
+            if (deathSound != null)
+                deathSound.Play();
+
+            GetComponent<Collider>().enabled = false;
+
+            float delay = deathEffect ? deathEffect.main.duration : 0f;
+            Destroy(gameObject, delay);
         }
     }
 }
